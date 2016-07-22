6b3facf1d2721a4c81f41d34d2a9d26ca1cad815
Kurtis Kemple <kurtiskemple@gmail.com>
2016-07-22 07:54:51 -0400

    add theme

---
+++ index.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const foregroundColor = '#eff0eb';
+const backgroundColor = '#1d262f';
+const black = backgroundColor;
+const slate = '#495f79';
+const lightSlate = '#56708f';
+const blue = '#7cb5f9';
+const green = '#0acd93';
+const lightGreen = '#52ffc8';
+const gray = '#8ca59d';
+const brightBlack = '#8ca59d';
+const brightWhite = foregroundColor;
+
+exports.decorateConfig = config => {
+	return Object.assign({}, config, {
+		backgroundColor,
+		foregroundColor,
+		borderColor: '#222430',
+		cursorColor: '#97979b',
+		colors: [
+			black,
+			slate,
+      lightSlate,
+      blue,
+			green,
+			lightGreen,
+			gray,
+
+			// bright
+			brightBlack,
+			slate,
+      lightSlate,
+      blue,
+			green,
+			lightGreen,
+			brightWhite
+		],
+		css: `
+			${config.css || ''}
+			.tab_active:before {
+				border-color: rgba(10, 205, 147, 0.25);
+			}
+		`
+	});
+};
---
+++ readme.md
@@ -0,0 +1,14 @@
+# hyperterm-duotone-darksea
+
+> Duotone Dark Sea [HyperTerm](https://hyperterm.org) theme
+
+Inspired by the [Duotone](http://simurai.com/projects/2016/01/01/duotone-themes/) themes by [Simurai](http://simurai.com/).
+
+## Install
+
+Add `hyperterm-duotone-darksea` to the plugins list in your `~/.hyperterm.js` config file.
+
+
+## License
+
+MIT © [Kurtis Kemple](https://twitter.com/kurtiskemple)
